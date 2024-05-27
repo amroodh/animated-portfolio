950e5e50d02becd2689f326c8e3986698cec8262
Amrit Raj <amritraj.ec21@rvce.edu.in>
2024-05-28 01:02:56 +0530

    changes are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import "./app.scss"
+import Navbar from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section id="Homepage">
+    <Navbar/>
+    </section>
+    <section id="Services">Parallax</section>
+    <section>Services</section>
+    <section id="Portfolio">Parallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section id="Contact">Contact</section>
+    <section id="About">About</section>
+  </div>;
 };
 
 export default App;
