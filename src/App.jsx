d5a762629f2960de8c16b3880cbe521ee4a9dd29
Amrit Raj <amritraj.ec21@rvce.edu.in>
2024-05-28 11:05:50 +0530

    changes are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./app.scss"
 import Navbar from "./components/navbar/Navbar";
-
+import Hero from "./components/navbar/hero/Hero";
 const App = () => {
   return <div>
     <section id="Homepage">
     <Navbar/>
+    <Hero/>
     </section>
     <section id="Services">Parallax</section>
     <section>Services</section>
