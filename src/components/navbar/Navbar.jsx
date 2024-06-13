790b3e323f991ba9351406fa5ca9eb383f9ce750
Amrit Raj <amritraj.ec21@rvce.edu.in>
2024-06-13 21:11:18 +0530

    changes are made

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,32 +1,37 @@
-import Sidebar from "./Sidebar/Sidebar"
-import "./navbar.scss"
-import { motion } from "framer-motion"
+import Sidebar from "./Sidebar/Sidebar";
+import "./navbar.scss";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <div className = "navbar">
-        {/* Sidebar */}
-        <Sidebar/>
-        <div className="wrapper">
-            <motion.span
-                initial={{ opacity: 0,scale: 0.5 }}
-                animate={{ opacity: 1,scale: 1}}
-                transition={{ duration: 0.5}}
-                >Amrit Raj
-                </motion.span>
-            <div className="social">
-                <a href="#"><img src="/facebook.png" alt=""/></a>
-                <a href="#"><img src="/instagram.png" alt=""/></a>
-                <a href="#"><img src="/github.png" alt=""/></a>
-                <a href="#"><img src="/linkedin.png" alt=""/></a>
-
-
-            </div>
-
+    <div className="navbar">
+      {/* Sidebar */}
+      <Sidebar />
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Amrit Raj
+        </motion.span>
+        <div className="social">
+          <a href="https://leetcode.com/u/user9393ZG/">
+            <img src="/leetcode.png" alt="" />
+          </a>
+          <a href="https://www.codechef.com/users/hacker_ar02">
+            <img src="/codechef.png" alt="" />
+          </a>
+          <a href="https://github.com/amroodh">
+            <img src="/github.png" alt="" />
+          </a>
+          <a href="https://www.linkedin.com/in/amrit-raj-3693051ba/">
+            <img src="/linkedin.png" alt="" />
+          </a>
         </div>
-      
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
